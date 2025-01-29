a16557b704459caf20b65f599e0ddcaefff7c27a
Piyush Zala <dev.piyushzala@gmail.com>
2025-01-29 22:35:12 +0530

    upda : database integration

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,16 @@
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
+import { SupabaseAdapter } from "@auth/supabase-adapter";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
-})
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  adapter: SupabaseAdapter({
+    url: process.env.SUPABASE_URL!,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY!,
+  })
+});
