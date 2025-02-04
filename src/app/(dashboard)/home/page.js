bd027fefffa5e1514aff34a8948965023f7a380e
Piyush Zala <dev.piyushzala@gmail.com>
2025-02-04 22:24:03 +0530

    fix:build issue

--- src/app/(dashboard)/home/page.js
+++ src/app/(dashboard)/home/page.js
@@ -2,7 +2,7 @@ import { getAllStreamsAction } from "@/app/actions/audio";
 import Image from "next/image";
 
 export default async function Home() {
-  const res = await getAllStreamsAction();
-  console.log("res: ", res);
+  // const res = await getAllStreamsAction();
+  // console.log("res: ", res);
   return <div></div>;
 }
