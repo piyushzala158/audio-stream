dd6030fefb8ab904fa98dfd19b8f50d76a0ac847
Piyush Zala <dev.piyushzala@gmail.com>
2025-02-08 16:38:40 +0530

    upda : added meta data

--- src/app/(dashboard)/stream/[id]/page.jsx
+++ src/app/(dashboard)/stream/[id]/page.jsx
@@ -1,13 +1,24 @@
 import { getStreamDetailsAction } from "@/app/actions/audio";
 import StremDetails from "@/views/stream/StremDetails";
-import React from "react";
 
 const page = async ({ params }) => {
   const id = await params.id;
-
   const res = await getStreamDetailsAction(id);
 
   return <StremDetails data={res.stream} />;
 };
 
 export default page;
+
+export async function generateMetadata({ params }) {
+  // read route params
+  const id = (await params).id;
+
+  // fetch data
+  const data = await getStreamDetailsAction(id);
+
+  return {
+    title: data?.title,
+    description: data?.description,
+  };
+}
